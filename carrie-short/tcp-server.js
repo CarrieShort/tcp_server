14776f59ea50d5f54347068e35690311dc27e0b8
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-12 08:51:50 -0700

    add make directory functionality and add gitignore

--- carrie-short/tcp-server.js
+++ carrie-short/tcp-server.js
@@ -1,10 +1,14 @@
 const net = require ('net');
 const fs = require ('fs')
 
+var dir = './logs';
 
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
 
 const server = net.createServer((socket) => {
-  const writeToFile = fs.createWriteStream(__dirname + '/log_' + Date.now() + '.txt');
+  const writeToFile = fs.createWriteStream(__dirname + '/logs/log_' + Date.now() + '.txt');
   socket.pipe(writeToFile);
 });
 
