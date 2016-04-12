88294769179b52878cf886cea16178c5572cb253
CarrieShort <carrie.r.phillips@gmail.com>
2016-04-12 13:44:36 -0700

    add second test to count number of files

--- carrie-short/test/tcp-server-test.js
+++ carrie-short/test/tcp-server-test.js
@@ -1,11 +1,16 @@
 const net = require('net');
+const fs = require('fs');
 const chai = require('chai');
 const expect = chai.expect;
 const tcp = require(__dirname + '/../lib/tcp-server.js');
+var numFiles;
 
 describe('TCP Server connection test', () => {
   after(() => {
     tcp.close();
+  });
+  before(() => {
+    numFiles = fs.readdirSync(__dirname + '/../logs').length;
   });
   it('should receive a response \'request complete\' from server', (done) => {
 
@@ -25,5 +30,9 @@ describe('TCP Server connection test', () => {
       done();
     });
 
+  });
+  it('should create a new file in logs', () => {
+    var newNumFiles = fs.readdirSync(__dirname + '/../logs').length;
+    expect(numFiles + 1).to.eql(newNumFiles);
   });
 });
